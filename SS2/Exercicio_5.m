8ddbaefd1ce87d7842a8c1f5816d21405dfd637f
lesly <leslymontufarj@gmail.com>
2019-11-22 23:25:36 -0300

    info

--- SS2/Exercicio_5.m
+++ SS2/Exercicio_5.m
@@ -16,29 +16,13 @@ music.path = strcat(path, filename);
 music.player = audioplayer(music.audiodata, music.Fs);
 
 % Dados dos sinais
-voice.number_of_samples = size(voice.audiodata, 1);
-voice.maxtime = voice.number_of_samples/voice.Fs;
-voice.t = 0:voice.number_of_samples - 1; %discreto;
+voice.number_of_samples = size(voice.audiodata, 1); % 3.2s a 3.6s
+voice.n = 0:voice.number_of_samples - 1; % discreto;
+voice.t = voice.n/voice.Fs; % tempo tem o mesmo tam que audiodata matrix
+figure('Name', 'Voice'); plot(voice.t, voice.audiodata);
 info('Minha voz', voice);
 music.number_of_samples = size(music.audiodata, 1);
 info('Música', music);
-
-% Entrada de sons
-% fprintf('SELECT\nG - Play\nP - Pause\nR - Resume\nS - Stop\n\n')
-% while 1
-%     switch input('choice: ', 's')
-%         case 'G'
-%             play(voice.player);
-%         case 'P'
-%             pause(voice.player);
-%         case 'R'
-%             resume(voice.player);
-%         case 'S'
-%             stop(voice.player);
-%             break;
-%     end
-% end
-
 
 function info(title, audio)
     fprintf('%s\n', title);
@@ -46,4 +30,12 @@ function info(title, audio)
     fprintf('\tNúmero de amostras no audio: %g\n', amostras);
     fprintf('\tTaxa de amostragem em Hz: %g\n', audio.Fs); % a cada 1 segundo tem Fs amostras -> amostras/seg
     fprintf('\tTempo de duração: %g segundos\n\n', amostras/audio.Fs); %(amostras)*(seg/amostras) = seg -> amostras*1/Fs
+end
+
+function [audio] = signal_audio_plot(audio, type)
+    if srtcmp(type,'Time vs. Freq')
+        audio.number_of_samples = size(audio.audiodata, 1);
+        audio.n = 0:audio.number_of_samples - 1; % discreto;
+        audio.t = audio.n/audio.Fs; % tempo tem o mesmo tam que audiodata matrix
+    end
 end
