71cf93189ce339d6c829e6f5051ef30ec32f37f0
lesly <leslymontufarj@gmail.com>
2019-11-22 22:26:53 -0300

    usando sound

--- SS2/Exercicio_5.m
+++ SS2/Exercicio_5.m
@@ -1,13 +1,25 @@
+% Reset sounds
+clear sound; 
+clc;
+close all;
+clear all;
+
+% Recebe os dados
 [filename path] = uigetfile('*', 'Select voice file');
 voice.path = strcat(path, filename);
 [voice.audiodata, voice.Fs] = audioread(voice.path);
-sound(voice.audiodata, voice.Fs);
 
 [filename path] = uigetfile('*', 'Select voice file');
 music.path = strcat(path, filename);
-[music.audiodata, music.Fs] = audioread(music.path);
-sound(voice.audiodata, voice.Fs);
+[music.audiodata, music.Fs] = audioread(music.path); % audiodata is the a matrix mxn where n is the number of channels
 
-t = 0:1/Fs:1;
-figure;
-plot(t(1:length(y)), y);
+% Quero o histograma
+figure('Name', 'Voice: Histogram of AudioData');
+hist(voice.audiodata);
+figure('Name', 'Music: Histogram of AudioData');
+hist(music.audiodata);
+
+% Entrada de sons
+sound(voice.audiodata, voice.Fs);
+sound(music.audiodata, music.Fs);
+
