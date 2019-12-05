29e6120069b6b1ae343a7f07a3aeabd5ded0bc78
lesly <leslymontufarj@gmail.com>
2019-12-05 18:15:53 -0300

    n é numero de amostras: passo 1 !!!

--- SS2/lista 2/EX1.m
+++ SS2/lista 2/EX1.m
@@ -1,27 +1,28 @@
-clear all; clc;
+clear all; close all; clc;
 
 % Invervalo a ser analisado - beep beep beep
 ti = 0;
-tf = 0.002;
+tf = 0.0018;
 
 % Sinal recebido
 syms t real
 x = 5*sin(2*pi*1000*t) + 2*cos(2*pi*3000*t) + 0.5*cos(2*pi*5000*t);
 figure('Name', 'Receptor'); subplot(2,1,1); ezplot(x, [ti, tf])
-title('Sinal Recebido');
-set(gca,'FontSize',14);
+title('Sinal Recebido', 'FontSize',14);
+ylabel('$x(t)$','Interpreter','LaTex','FontSize',18);
 
 % Conversor A/D: Amostragem
 Fs = 15e3; % Fs > 2*Fmax_sinal (Teorema de Nyquist para evitar a amostragem)
 Ts = 1/Fs; % esquece no tempo t
-n = 0:Ts:tf/Ts; % eixo tempo amostrado
+n = ti/Ts:1:tf/Ts; % numero de amostras depende de [ti tf]
 
 x_n = 5*sin(2*pi*1000*n/Fs) + 2*cos(2*pi*3000*n/Fs) + 0.5*cos(2*pi*5000*n/Fs);
-subplot(2,1,2); plot(n, x_n);
+subplot(2,1,2); scatter(n, x_n);
 title('Pós Conversor A/D com Fs=12kHz');
-xlabel('$t$','Interpreter','LaTex','FontSize',18);
-ylabel('$x[nT_s],x(t)$','Interpreter','LaTex','FontSize',18);
+xlabel('$n$','Interpreter','LaTex','FontSize',18);
+ylabel('$x[nT_s]$','Interpreter','LaTex','FontSize',18);
 set(gca,'FontSize',14);
+
 
 
 
