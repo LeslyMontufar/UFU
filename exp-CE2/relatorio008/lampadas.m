f364840c822e192fd63e47e9ffb2ba5cbde3967e
lesly <leslymontufarj@gmail.com>
2019-12-09 09:06:22 -0300

    an small but constructive report

--- exp-CE2/relatorio008/lampadas.m
+++ exp-CE2/relatorio008/lampadas.m
@@ -5,8 +5,18 @@ array = table2array(table);
 
 h = figure('Name', 'Dados Recebidos');
 plot(array(:,1), array(:,2));
-title('Sinal não-senoidal sobre o resistor acoplado ao LED');
+title('Sinal sobre o resistor acoplado ao LED');
 xlabel('$t$','Interpreter','LaTex','FontSize',16);
 ylabel('$V_{LED}$ (V)','Interpreter','LaTex','FontSize',16);
 
-saveas(h, 'Sinal_IN.png');
+% saveas(h, 'Sinal_IN.png');
+
+espectro = fft(array(:, 2));
+g = figure('Name','Espectro do Sinal Recebido'); 
+stem(abs(espectro));
+title('Espectro da corrente sobre o resistor');
+xlabel('$t$','Interpreter','LaTex','FontSize',16);
+ylabel('$V_{LED}$ (V)','Interpreter','LaTex','FontSize',16);
+xlim([0 120]);
+
+saveas(g, 'Espectro_IN.png');
