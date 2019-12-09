7ffc7d9682546a485aec0ef4642178ad5cdcece0
lesly <leslymontufarj@gmail.com>
2019-12-09 12:26:18 -0300

    organiza

--- SS2/lista 2/EX1/EX1.m
+++ SS2/lista 2/EX1/EX1.m
@@ -1,33 +1,113 @@
-clear all; clc;
+clear all; clc; clear t_f
 
 % Invervalo a ser analisado - beep beep beep
 ti = 0;
-tf = 0.0018;
+t_f = 0.0018;
+t_total = t_f-ti;
 
 % Sinal recebido
 syms t real
-x = 5*sin(2*pi*1000*t) + 2*cos(2*pi*3000*t) + 0.5*cos(2*pi*5000*t);
-figure('Name', 'Receptor'); ezplot(x, [ti, tf])
-title('Sinal Recebido', 'FontSize',14);
-ylabel('$x(t)$','Interpreter','LaTex','FontSize',18); hold on;
+xt = 5*sin(2*pi*1000*t) + 2*cos(2*pi*3000*t) + 0.5*cos(2*pi*5000*t);
 
-% Conversor A/D: Amostragem
-Fs = 15e3; % Fs > 2*Fmax_sinal (Teorema de Nyquist para evitar a amostragem)
-Ts = 1/Fs; % esquece no tempo t
-n = ti/Ts:1:tf/Ts; % numero de amostras depende de [ti tf]
-t_sample = ti:Ts:length(n)*Ts;
+% Amostragem
+Fs = 20e3; % Fs > 2*Fmax_sinal (Teorema de Nyquist para evitar o aliasing)
+Ts = 1/Fs; 
+figure('Name', 'Receptor');
+subplot(2, 1, 1);
+[x_n, n] = figure_amostragem('$nT_s$','$x[nT_s]$', xt, t, Fs, ti, t_f);
+title(strcat('Sinal recebido pós amostragem com Fs=', string(Fs/1e3), 'kHz'));
+ax = gca; ax.FontSize=12;
 
-x_n = 5*sin(2*pi*1000*n/Fs) + 2*cos(2*pi*3000*n/Fs) + 0.5*cos(2*pi*5000*n/Fs);
-% Converteu o sinal em digital normalizando por /Fs
+% Espectro de frequencia do sinal amostrado
+X = fft(x_n);
+X_abs = abs(X);
+X_phased = phase(X)*(180/pi);
+w = n/(t_f-ti);% frequencia em Hz % usou Fs
+subplot(2,1,2); stem(w, X_abs);
+title('Análise do Espectro de Frequência do Sinal Digital');
+ax = gca; ax.FontSize=12; ax.XTick = 0:3e3:Fs;
 
-scatter(Ts.*n, x_n);
-title('Pós Conversor A/D com Fs=15kHz');
-xlabel('$n$','Interpreter','LaTex','FontSize',18);
-ylabel('$x[nT_s]$','Interpreter','LaTex','FontSize',18);
+% Projeto do filtro rejeita banda
+figure('Name','Projeto do filtro rejeita banda'); 
+freq_rejeitada = 3e3; 
+w_rejeitada = 2*pi*freq_rejeitada/Fs; % Freq Digital equivalente
+zero = 0.98*(cos(w_rejeitada)+ 1j*sin(w_rejeitada));
+zeros = [zero; conj(zero)];
+polo = 0.80*real(zero)+1j*imag(zero);
+polos = [polo; conj(polo)];
+k = 0.2/(5*.5);
+[Hjw] = H(zeros, polos, k, w);
 
+subplot(2, 1, 1); polarplot([zeros polos], '*');
+title('Diagrama de polos e zeros do Filtro Seletivo');
+subplot(2, 1, 2); stem(w, abs(Hjw));
+title('Espectro em freq da Resposta ao Impulso');
+xlabel('$\omega$','Interpreter','LaTex','FontSize',14);
+ylabel('$|H(jw)|$','Interpreter','LaTex','FontSize',14);
+ax = gca; ax.FontSize=12; ax.XTick = 0:1e3:Fs;
 
+% Para encontrar a saída filtrada tem-se:
+Y = Hjw.* X;
+Y_abs = abs(Y);
+Y_phased = phase(Y)*(180/pi);
 
+figure('Name','Sinal Transmitido');
+subplot(2, 1, 1); stem(w, Y_abs);
+title('Espectro em freq da saída');
+xlabel('$\omega$','Interpreter','LaTex','FontSize',18);
+ylabel('$|Y(jw)|$','Interpreter','LaTex','FontSize',18);
 
+yt = ifft(Y_abs);
+yt_abs = real(yt);
+subplot(2, 1, 2); stem(n*Ts, yt_abs);
+title('Espectro no tempo da saída');
+xlabel('$t$','Interpreter','LaTex','FontSize',18);
+ylabel('$y(t)$','Interpreter','LaTex','FontSize',18);
 
+hold on;
+xt_filtro_ideal = k*(5*sin(2*pi*1000*t) + 0.5*cos(2*pi*5000*t));
+ezplot(xt_filtro_ideal, [ti, t_f]);
 
+% Functions
 
+function [s, n] = amostragem(sinal, var, Fs, ti, t_f)
+    % AMOSTRAGEM (sinal, var, Fs, ti, t_f)
+    % sinal: em funcao de var
+    % var: variavel de percurso analógico
+    % Fs: frequencia de amostragem desejada
+    % ti: tempo inicial
+    % t_f: tempo final
+    
+    n = ti*Fs:1:t_f*Fs; % Ts=1/Fs
+    s = eval(strrep(string(sinal),char(var),'n/Fs'));
+end
+
+function [s_n, n]= figure_amostragem(x_label, y_label, sinal, var, Fs, ti, t_f)
+    % FIGURE_AMOSTRAGEM (x_label, y_label, sinal, var, Fs, ti, t_f)
+    % x_label: str
+    % y_label: str
+    % sinal: em funcao de var
+    % var: variavel de percurso analógico
+    % Fs: frequencia de amostragem desejada
+    % ti: tempo inicial
+    % t_f: tempo final
+    
+    [s_n, n] = amostragem(sinal, var, Fs, ti, t_f);
+    ezplot(sinal, [ti t_f]);
+    hold on;
+    scatter(n/Fs, s_n);
+    xlabel(x_label,'Interpreter','LaTex','FontSize',16);
+    ylabel(y_label,'Interpreter','LaTex','FontSize',16);
+    
+end
+
+function [Hjw] = H(zeros, polos, k, w)
+    r = 1;
+    Hjw = k;
+    for ii = 1:length(zeros)
+        Hjw = Hjw.*zeros(ii).*r.*exp(-1j*w);
+    end
+    for ii = 1:length(polos)
+        Hjw = Hjw./(zeros(ii).*r.*exp(-1j*w));
+    end
+end
